1af3e70cde864dea00df8d769d4b69e2ab01be22
Stoic <sme.nik.dov@gmail.com>
2024-12-26 16:51:09 +0500

    починил типы для flex

--- src/components/Flex/Flex.types.ts
+++ src/components/Flex/Flex.types.ts
@@ -2,10 +2,10 @@ import type React from 'react';
 import { BaseSizes } from '@/typings';
 
 export interface FlexBaseProps extends React.HTMLAttributes<HTMLElement> {
-    direction?: React.CSSProperties['flexDirection'];
-    wrap?: React.CSSProperties['flexWrap'];
-    justify?: React.CSSProperties['justifyContent'];
-    align?: React.CSSProperties['alignItems'];
+    direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+    wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+    justify?: 'flex-start' | 'flex-end' | 'center' | 'space-around' | 'space-between' | 'space-evenly';
+    align?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
     gapX?: BaseSizes;
     gapY?: BaseSizes;
     gap?: React.CSSProperties['gap'];
